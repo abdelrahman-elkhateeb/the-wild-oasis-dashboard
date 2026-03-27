9a1eef2f19878034ef840744aa19d1520b53b746
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2026-03-27 19:32:33 +0200

    setting up pages and routes /app layout/ sidbar and main nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard"
+import Bookings from "./pages/Bookings"
+import Cabins from "./pages/Cabins"
+import Settings from "./pages/Settings"
+import Login from "./pages/Login"
+import PageNotFound from "./pages/PageNotFound"
+import GlobalStyles from "./styles/GlobalStyles";
+
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Dashboard />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
-export default App
+export default App;
