0d2eb7315249c3ad1f90d83335ebf4a04bce2ccc
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2026-03-28 22:08:00 +0200

    setup the main page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Settings from "./pages/Settings"
 import Login from "./pages/Login"
 import PageNotFound from "./pages/PageNotFound"
 import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
 
 
 function App() {
@@ -14,16 +15,18 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="account" element={<Dashboard />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Dashboard />} />
+          </Route>
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter >
     </>
   )
 }
