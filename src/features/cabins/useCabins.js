890a1879ced8e8196bc62815dbee2b0c66bae96c
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2026-04-01 17:17:29 +0200

    Update useCabins.js

--- src/features/cabins/useCabins.js
+++ src/features/cabins/useCabins.js
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query"
 import { getCabins } from "../../services/apiCabins"
 
 export function useCabins() {
-  const { isPending, data: cabins, error } = useQuery({
+  const { isPending, data: cabins = [], error } = useQuery({
     queryKey: ["cabins"],
     queryFn: getCabins
   })
