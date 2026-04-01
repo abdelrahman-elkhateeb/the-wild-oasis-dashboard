02fb2ad967b7e05ce1f698f6324dd29c0ae2ecca
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2026-04-01 17:53:20 +0200

    fetch settings data at setting page

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,9 @@
-
 import { createClient } from '@supabase/supabase-js'
+
 export const supabaseUrl = 'https://wfcrqynuwpbqfcsbmfbb.supabase.co'
+
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmY3JxeW51d3BicWZjc2JtZmJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MTA0MDgsImV4cCI6MjA5MDE4NjQwOH0.EW0ZLzeFG9fhvfGL_LxTO3jmWiCQO_neNsy3ioaSFig"
+
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
